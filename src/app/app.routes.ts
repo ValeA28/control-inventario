72051eba2b3033c61c78267ec5e19a30df7c6202
ValeA28 <valearabale2809@gmail.com>
2026-07-29 01:45:33 -0300

    Actualizacion de rutas con authGuard y redespliegue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
-// Asegúrate de importar tu componente del dashboard. 
-// Si está en 'components/dashboard/dashboard.component', ajusta la ruta:
 import { DashboardComponent } from './components/dashboard/dashboard';
 import { RegistroComponent } from './components/registro/registro';
 import { CheckoutComponent } from './components/checkout/checkout';
@@ -11,6 +9,6 @@ export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
-  { path: 'checkout', component: CheckoutComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'checkout', component: CheckoutComponent, canActivate: [authGuard] }
 ];
